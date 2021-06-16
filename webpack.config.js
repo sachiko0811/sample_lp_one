ed66246ed00172148a9cc632e591b84366d7fa81
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-15 23:32:18 -0700

    json webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/javascripts/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'javascripts/main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                targets: '> 0.25%, not dead'
+                            }],
+                        ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(css|sass|scss)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: false,
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg)/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                },
+                use: [
+                    // {
+                    //     loader: 'file-loader',
+                    //     options: {
+                    //         esModule: false,
+                    //         name: 'images/[name].[ext]',
+                    //     },
+                    // },
+                ],
+            },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './stylesheets/my.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/index.pug',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/members/sachiko.pug',
+            filename: 'members/sachiko.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+}
